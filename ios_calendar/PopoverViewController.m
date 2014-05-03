117f894d4828d118e859878128894a7c5248bb56
Maxim Bilan <maximb.mail@gmail.com>
2014-05-03 20:18:16 +0300

    Fixed calling of calendar delegate methods in the popover class

--- ios_calendar/PopoverViewController.m
+++ ios_calendar/PopoverViewController.m
@@ -10,13 +10,32 @@
 
 @interface PopoverViewController ()
 
+@property (weak, nonatomic) IBOutlet CalendarView *calendarView;
+
 @end
 
 @implementation PopoverViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.calendarView.calendarDelegate = self;
+}
+
 - (void)didChangeCalendarDate:(NSDate *)date
 {
     NSLog(@"didChangeCalendarDate:%@", date);
+}
+
+- (void)didChangeCalendarDate:(NSDate *)date withType:(NSInteger)type withEvent:(NSInteger)event
+{
+    NSLog(@"didChangeCalendarDate:%@ withType:%d withEvent:%d", date, type, event);
+}
+
+- (void)didDoubleTapCalendar:(NSDate *)date withType:(NSInteger)type
+{
+    NSLog(@"didDoubleTapCalendar:%@ withType:%d", date, type);
 }
 
 @end
