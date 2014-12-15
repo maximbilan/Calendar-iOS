20b6207574b22535e26a9a0dcb798e12b026a807
Maxim Bilan <maximb.mail@gmail.com>
2014-12-15 23:43:29 +0200

    Code review

--- ios_calendar/Sources/NSDate+CalendarView.m
+++ ios_calendar/Sources/NSDate+CalendarView.m
@@ -14,7 +14,6 @@
 {
 	NSCalendar *currentCalendar = [NSCalendar currentCalendar];
 	NSRange daysRange = [currentCalendar rangeOfUnit:NSDayCalendarUnit inUnit:NSMonthCalendarUnit forDate:self];
-	
 	return daysRange.length;
 }
 
@@ -24,14 +23,16 @@
 
 	NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
 	[calendar setTimeZone:timeZone];
-	NSDateComponents *components = [calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) fromDate:self];
+	NSDateComponents *components = [calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit)
+                                               fromDate:self];
 	[components setDay:1];
 	[components setHour:0];
 	[components setMinute:0];
 	[components setSecond:0];
 	[components setTimeZone:timeZone];
 	
-	NSDateComponents *weekdayComponents = [calendar components:(NSDayCalendarUnit | NSWeekdayCalendarUnit) fromDate:[calendar dateFromComponents:components]];
+	NSDateComponents *weekdayComponents = [calendar components:(NSDayCalendarUnit | NSWeekdayCalendarUnit)
+                                                      fromDate:[calendar dateFromComponents:components]];
 	NSInteger weekday = [weekdayComponents weekday];
 	
 	return (weekday == 1) ? 7 : weekday - 1;
